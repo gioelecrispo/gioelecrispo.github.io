e4e34533fcfb8bc3e813e1dfc12f2cc108d63da0
gioelecrispo <gioelecrispo@users.noreply.github.com>
2021-04-14 21:22:44 +0000

    deploy: 00b8a916f1d527caa7b47f5d9341ac025f0cad64

--- _nuxt/static/1618413976/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fapplications","\u002Fprojects","\u002F","\u002Fblog","\u002Fcv","\u002Fblog\u002Fmlops","\u002Fblog\u002Fsolid-rules","\u002Fblog\u002Fwor-talk"]})
