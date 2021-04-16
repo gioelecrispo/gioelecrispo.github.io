9d9ff8c9bbb76eec4bc63e94e3d370a068c00885
gioelecrispo <gioelecrispo@users.noreply.github.com>
2021-04-16 12:31:05 +0000

    deploy: 93892a48b013299918755a5b966ec7edf696e318

--- _nuxt/static/1618570136/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fblog","\u002Fcv","\u002Fprojects","\u002Fapplications","\u002Fblog\u002Fs-o-l-i-d-rules-to-build-a-solid-software-architecture","\u002Fblog\u002Fmitigate-risk-and-enhance-productivity-with-mlops","\u002Fblog\u002Fa-novel-writing-order-recovery-approach-for-handwriting-specimens"]})
