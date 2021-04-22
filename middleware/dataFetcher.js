a9f4a9eaeffce8a2454e13cb3688f2643b96cf36
Gioele Crispo <gioele.crispo@nttdata.com>
2021-04-22 10:58:52 +0200

    middleware fetching fix

--- middleware/dataFetcher.js
+++ middleware/dataFetcher.js
@@ -1,44 +1,41 @@
 export default async ({ store, $axios, $content }) => {
-    let promises = [];
-    console.log("MIDDLEWARE fetching");
-    let dataState = store.state["DataState"];
-    if (!dataState.githubUserInfo) {
-        console.log("FETCHING github userInfo");
-        promises.push($axios({
-            url: "https://api.github.com/users/gioelecrispo",
-            method: "get"
+    let promises = []
+    console.log('MIDDLEWARE fetching')
+    let dataState = store.state['DataState']
+    console.log('FETCHING github userInfo')
+    promises.push($axios({
+            url: 'https://api.github.com/users/gioelecrispo',
+            method: 'get'
         })
             .then(success => {
                 store.dispatch('DataState/setGithubUserInfo', success.data)
             })
             .catch(err => {
-                console.error("setGithubUserInfo - Too Much Requests", err);
+                console.error('setGithubUserInfo - Too Much Requests', err)
             })
-        );
-    }
-    if (!dataState.githubProjects) {
-        console.log("FETCHING github projects");
-        promises.push($axios({
-            url: "https://api.github.com/users/gioelecrispo/repos",
-            method: "get"
+    )
+
+    console.log('FETCHING github projects')
+    promises.push($axios({
+            url: 'https://api.github.com/users/gioelecrispo/repos',
+            method: 'get'
         })
             .then(success => {
                 store.dispatch('DataState/setGithubProjects', success.data)
             })
             .catch(err => {
-                console.error("setGithubProjects - Too Much Requests", err);
+                console.error('setGithubProjects - Too Much Requests', err)
             })
-        );
-    }
+    )
+
     if (!dataState.blogArticles) {
         $content('blog')
             .only(['title', 'description', 'img', 'slug', 'tags', 'createdAt', 'updatedAt'])
             .sortBy('createdAt', 'desc')
             .fetch()
             .then(articles => {
-                store.dispatch('DataState/setBlogArticles', articles);
-            });
-
+                store.dispatch('DataState/setBlogArticles', articles)
+            })
     }
-    return Promise.all(promises);
+    return Promise.all(promises)
 }
